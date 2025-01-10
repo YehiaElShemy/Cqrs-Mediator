0b5a8e3d3df5193d67554c11c616e7f95e1fdb7b
yehiazakaria259 <yehiazakaria259@gmail.com>
2025-01-10 10:10:27 +0200

    edit

--- Cqrs_Mediator.InfraStructure/HttpClientRepositry/HttpClientServeices.cs
+++ Cqrs_Mediator.InfraStructure/HttpClientRepositry/HttpClientServeices.cs
@@ -16,6 +16,7 @@ namespace Cqrs_Mediator.InfraStructure.HttpClientRepositry
     public class HttpClientServeices : IHttpClientServeices
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IHttpClientFactory _httpClientFactory2;
         public HttpClientServeices(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
